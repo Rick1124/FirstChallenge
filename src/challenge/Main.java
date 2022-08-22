350a9df1150da9916eb7158d8feefafdd6e3e246
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-22 13:36:12 -0500

    1.1

--- src/challenge/Main.java
+++ src/challenge/Main.java
@@ -1,2 +1,35 @@
-package challenge;public class Main {
+package challenge;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 6, 23, 10, 1, 3};
+        String sol = solution(arr);
+        System.out.println(sol);
+
+    }
+
+    public static String solution(int[] arr){
+        Arrays.sort(arr);
+        int sum=0;
+        boolean answer;
+
+        for (int i = 0; i < arr.length - 1 ; i++) {
+            sum += arr[i];
+        }
+
+        if (sum == arr[arr.length - 1]){
+            return "true";
+        }
+        else {
+            return "false";
+        }
+        //answer = sum == arr[arr.length - 1];
+        //System.out.println(answer);
+
+
+
+    }
 }
